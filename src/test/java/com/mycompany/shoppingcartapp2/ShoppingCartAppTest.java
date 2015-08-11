b7e02d5e135f109c463f47f5dbce61f95e85ead9
annygil <annyvanessag@gmail.com>
2015-08-11 11:29:42 -0430

    Primer build

--- src/test/java/com/mycompany/shoppingcartapp2/ShoppingCartAppTest.java
+++ src/test/java/com/mycompany/shoppingcartapp2/ShoppingCartAppTest.java
@@ -30,43 +30,7 @@ public class ShoppingCartAppTest {
                 Assert.assertEquals(30.0,cart.getTotalCartValue(),0.0);
 	}
         
-        	@Test
-	public void addDifferentProductsToTheCart(){
-                IOffer offer =new BuyXItemGetYItemFreeOffer(2,1);
-		ShoppingCart cart = new ShoppingCart();
-		Product gatsByCream = new Product("Gatsby hair cream", 3, 90.0);
-		Product bvlgiriSoap = new Product("Bvlgiri Soap", 2, 200.0);
-                cart.setOffer(offer);
-		cart.addProduct(gatsByCream);
-		cart.addProduct(bvlgiriSoap);
-		Assert.assertEquals(2, cart.getProductCount());
-		Assert.assertEquals(260.0, cart.getTotalCartValue(),0.0);
-	}
-	
-	@Test
-	public void testAddMultipleQuantityOfAProductAndApplyOfferToCart() {
-		IOffer offer = new BuyXItemGetYItemFreeOffer(2,1);
-		ShoppingCart cart = new ShoppingCart();
-                cart.setOffer(offer);
-		Product product = new Product("Gatsby hair cream", 5, 150.0);
-		cart.addProduct(product);
-		Assert.assertEquals(1, cart.getProductCount());
-		Assert.assertEquals(120.0, cart.getTotalCartValue(),0.0);
-	}
-        
-  
 
-   @Test
-	public void testApplyBuyOneGetFiftyPercentDiscountOnNextOfferToTheCart(){
-		IOffer offer = new DiscountOnNextItemOffer(50.0);
-		ShoppingCart cart = new ShoppingCart();
-		cart.setOffer(offer);
-		Product gatsByCream = new Product("Gatsby hair cream", 2, 60.0);
-		cart.addProduct(gatsByCream );
-		Assert.assertEquals(1, cart.getProductCount());
-		Assert.assertEquals(45.0, cart.getProductByName("Gatsby hair cream").getTotalPrice(),0.0);
-		Assert.assertEquals(45.0, cart.getTotalCartValue(),0.0);
-	}
 }
 
 
